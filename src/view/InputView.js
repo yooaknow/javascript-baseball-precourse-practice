db44d6e8c77db52d61e6f8d4c1efd758a74bc813
yooaknow <dbdkfma1228@naver.com>
2026-01-05 21:15:59 +0900

    feat: 숫자 야구 시작 안내 및 숫자 입력 받는 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -7,3 +7,15 @@
 게임의 도입부의 Input을 문자열로 받아야함. 
 
 */
+
+import { Console } from "@woowacourse/mission-utils";
+
+class InputView {
+  async readCount() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const inputCount = await Console.readLineAsync("숫자를 입력해주세요 :");
+    return inputCount;
+  }
+}
+
+export default InputView;
