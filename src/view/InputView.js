2b015cd1787d676ff0e1770296c4eec203b292f3
yooaknow <dbdkfma1228@naver.com>
2026-01-05 21:17:52 +0900

    fix: 숫자 입력 재사용을 위해 내부 로직 분리

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -11,8 +11,10 @@
 import { Console } from "@woowacourse/mission-utils";
 
 class InputView {
-  async readCount() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+  async Start() {
+    Console.print("숫자 야구 게임을 시작합니다.");}
+
+    async readCount() {
     const inputCount = await Console.readLineAsync("숫자를 입력해주세요 :");
     return inputCount;
   }
