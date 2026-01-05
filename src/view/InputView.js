28f01d4996f5d852d15303fcce3d5ad64cf364eb
yooaknow <dbdkfma1228@naver.com>
2026-01-05 21:19:28 +0900

    feat: 게임 새로 시작 기능 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -18,6 +18,11 @@ class InputView {
     const inputCount = await Console.readLineAsync("숫자를 입력해주세요 :");
     return inputCount;
   }
+
+   async readReplay() {
+    const readReplay = await Console.readLineAsync("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    return Number(readReplay);
+  }
 }
 
 export default InputView;
